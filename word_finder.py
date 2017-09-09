4d29f487c6257ad776edf20435dfb8b402b0a116
Nicolai Garcia <nicogarcia@Nicolais-MBP.attlocal.net>
2017-09-08 17:24:33 -0700

    Program expanded to show words with less letters than initial input. New en.txt file added to check letter combinations, contains more realistic collection of words. Added exit message.

--- word_finder.py
+++ word_finder.py
@@ -1,17 +1,20 @@
 """
 word_finder:
+
 given an input word, find all of the words that can be 
 made using its letters.
 Example
 INPUT:
-melon
+toy
 OUTPUT:
-lemon
-lone (eventually)
+to
+toy
+oy
+yo
 """
 
 # Open words file
-words = open("words.txt", "r")
+words = open("en.txt", "r")
 
 # convert file into a set
 word_list = set(line.lower().strip() for line in words)
@@ -20,13 +23,18 @@ word_list = set(line.lower().strip() for line in words)
 # For each letter, find all possible letter combinations starting with that same letter
 # new = start letter
 # remaining = remaining letters
-
 def word_combinations(new, remaining):
 	if len(remaining) == 0 and new in word_list:
 			print new
+	# words with less letters than original word included
+	elif new in word_list:
+		print new
 	for i, c in enumerate(remaining):
+		# add a character from remaining characters to new combination
 		comb = new + c
+		# remove added character from remaining collection
 		left = remaining[:i] + remaining[i+1:]
+		# recurse with new combination and collection of remaining letters
 		word_combinations(comb, left)
 
 def find_words(intial):
@@ -34,8 +42,9 @@ def find_words(intial):
 		remaining = word[:i] + word[i+1:]
 		word_combinations(c, remaining)
 
-# Get input word
+# Get word inputs until user defers
 word = raw_input("Word: (Enter to quit) ")
 while word is not "":
 	find_words(word)
 	word = raw_input("Word: ")
+print "Exiting... Thank you!"
